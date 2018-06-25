d72d7a2eeac503e4185053030efde11ce3c575f3
Missaka Iddamalgoda <m.iddamalgoda@gmail.com>
2018-06-25 23:59:25 +0530

    Database Script updated. Fixing on Librarian.

--- web/librarian.php
+++ web/librarian.php
@@ -113,14 +113,15 @@
 
                     <ul class="nav navbar-nav navbar-right">
                         <li class="">
-                            <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                            <a href="javascript:" class="user-profile dropdown-toggle" data-toggle="dropdown"
+                               aria-expanded="false">
                                 <img src="images/img.jpg" alt="">John Doe
                                 <span class=" fa fa-angle-down"></span>
                             </a>
                             <ul class="dropdown-menu dropdown-usermenu pull-right">
-                                <li><a href="javascript:;"> Profile</a></li>
+                                <li><a href="javascript:"> Profile</a></li>
                                 <li>
-                                    <a href="javascript:;">
+                                    <a href="javascript:">
                                         <span class="badge bg-red pull-right">50%</span>
                                         <span>Settings</span>
                                     </a>
@@ -137,7 +138,7 @@
         <!-- page content -->
         <div class="right_col" role="main">
             <!-- top tiles -->
-            <div class="row tile_count">
+            <!--<div class="row tile_count">
                 <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
                     <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
                     <div class="count">2500</div>
@@ -168,7 +169,7 @@
                     <div class="count">7,325</div>
                     <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>
                 </div>
-            </div>
+            </div>-->
             <!-- /top tiles -->
 
 
