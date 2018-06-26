2f55d2a0f27c18cbf73f26e8b5a2388e2467b8ca
Missaka Iddamalgoda <m.iddamalgoda@gmail.com>
2018-06-26 14:38:18 +0530

    Course model completed

--- model/Course.php
+++ model/Course.php
@@ -78,8 +78,63 @@ class Course
         if ($resultSet->num_rows > 0) {
             $arr = array();
             while ($row = $resultSet->fetch_assoc()) {
-                $bo = new \Course\CourseBO($row["CourseCode"], $row["Name"], $row["CreditHours"], $row["College"],
-                    $row["PreRequisteCourseCode"], $row["DepartmentCode"]);
+                $bo = new \Course\CourseSectionBO($row["CourseCode"], $row["SectionNumber"], $row["Semester"], $row["Year"],
+                    $row["Classroom"], $row["ClassTime"], $row["ClassSize"], $row["ProfessorID"]);
+                array_push($arr, $bo);
+            }
+            return $arr;
+        } else {
+            return NULL;
+        }
+    }
+
+
+    public function createCourseEnrollment(\Course\CourseEnrollmentBO $course): bool
+    {
+        $conn = DBConnection::getConnection();
+        $sql = "INSERT INTO CourseEnrollment VALUES ('{$course->getSid()}','{$course->getCourseSection()->getCourse()}'," .
+            "'{$course->getCourseSection()->getSectionNumber()}','{$course->getCourseSection()->getSemester()}'," .
+            "'{$course->getCourseSection()->getYear()}''{$course->getGrade()}')";
+        $result = $conn->query($sql);
+        if ($result === TRUE) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    public function getCourseEnrollmentsFor(\Course\CourseSectionBO $course): array
+    {
+        $conn = DBConnection::getConnection();
+        $sql = "SELECT * FROM CourseEnrollment WHERE CourseCode='{$course->getCourse()}' && SectionNumber='{$course->getSectionNumber()}'" .
+            "&& Semester={$course->getSemester()} && Year={$course->getYear()}";
+        $resultSet = $conn->query($sql);
+        if ($resultSet->num_rows > 0) {
+            $arr = array();
+            while ($row = $resultSet->fetch_assoc()) {
+
+                $bo = new \Course\CourseEnrollmentBO($row["StudentID"], $course, $row["Grade"]);
+                array_push($arr, $bo);
+            }
+            return $arr;
+        } else {
+            return NULL;
+        }
+    }
+
+    /*
+     * @returns an array with course enrollments along with
+     */
+    public function getCourseEnrollmentsBy(string $sid): array
+    {
+        $conn = DBConnection::getConnection();
+        $sql = "SELECT * FROM CourseEnrollment WHERE StudentID='{$sid}'";
+        $resultSet = $conn->query($sql);
+        if ($resultSet->num_rows > 0) {
+            $arr = array();
+            while ($row = $resultSet->fetch_assoc()) {
+                $courseSection = new \Course\CourseSectionBO($row["CourseCode"], $row["SectionNumber"], $row["Semester"], $row["Year"], 0, "", 0, "");
+                $bo = new \Course\CourseEnrollmentBO($row["StudentID"], $courseSection, $row["Grade"]);
                 array_push($arr, $bo);
             }
             return $arr;
